cc430fd9d05db535612a1e1d4b49aca32758e9cc
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-22 04:26:28 -0500

    Use shared autocommand group

--- lua/color-sync/init.lua
+++ lua/color-sync/init.lua
@@ -2,6 +2,8 @@ local M = {}
 
 function M.setup() end
 
+local group = vim.api.nvim_create_augroup("ColorSync", {})
+
 local wezterm = (vim.env.XDG_CONFIG_HOME or (assert(vim.env.HOME) .. "/.config")) .. "/wezterm"
 if vim.fn.isdirectory(wezterm) then
     local wezterm_colorschemes = {
@@ -14,10 +16,11 @@ if vim.fn.isdirectory(wezterm) then
 
     -- only register once in UI
     vim.api.nvim_create_autocmd("UIEnter", {
-        group = vim.api.nvim_create_augroup("WezTermColorscheme", {}),
+        once = true,
+        group = group,
         callback = function()
             vim.api.nvim_create_autocmd("ColorScheme", {
-                group = vim.api.nvim_create_augroup("WezTermColorscheme", {}),
+                group = group,
                 callback = function(args)
                     local colorscheme = wezterm_colorschemes[args.match]
                     if colorscheme then
