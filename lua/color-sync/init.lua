5520c2c512288d7157f6491cb487a600635c7d56
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-26 01:42:39 -0500

    Rework deferring to be handled by terminal tables

--- lua/color-sync/init.lua
+++ lua/color-sync/init.lua
@@ -2,8 +2,6 @@ local term = require("color-sync.terminal")
 
 local group = vim.api.nvim_create_augroup("ColorSync", {})
 
-local live_update_defer = nil
-local save_defer = nil
 local start_handle = nil
 local function register(colorscheme)
     vim.schedule(function()
@@ -11,10 +9,8 @@ local function register(colorscheme)
         if terminal then
             local terminal_colorscheme = terminal:get_colorscheme(colorscheme)
             if vim.v.vim_did_enter == 1 then
-                if live_update_defer ~= nil then live_update_defer:stop() end
-                if save_defer ~= nil then save_defer:stop() end
-                live_update_defer = vim.defer_fn(function() terminal:live_update(terminal_colorscheme) end, 400)
-                save_defer = vim.defer_fn(function() terminal:save(terminal_colorscheme) end, 800)
+                terminal:defer_live_update(terminal_colorscheme)
+                terminal:defer_save(terminal_colorscheme)
             else
                 if start_handle ~= nil then vim.api.nvim_del_autocmd(start_handle) end
                 start_handle = vim.api.nvim_create_autocmd("VimEnter", {
--- lua/color-sync/terminal/init.lua
+++ lua/color-sync/terminal/init.lua
@@ -5,12 +5,23 @@ local function new_terminal(id, name, live_update, save)
         id = id,
         name = name,
         live_update = function(_, terminal_colorscheme)
-            live_update(terminal_colorscheme)
+            live_update.callback(terminal_colorscheme)
+        end,
+        live_update_timeout = live_update.timeout,
+        defer_live_update = function(self, terminal_colorscheme)
+            if self.live_update_defer ~= nil then self.live_update_defer:stop() end
+            self.live_update_defer = vim.defer_fn(function() self:live_update(terminal_colorscheme) end,
+                self.live_update_timeout)
         end,
         save = function(self, terminal_colorscheme)
             vim.notify("Setting " .. tostring(self) .. " colorscheme to " .. (terminal_colorscheme or ""),
                 vim.log.levels.INFO)
-            save(terminal_colorscheme, vim.env.XDG_CONFIG_HOME or (assert(vim.env.HOME) .. "/.config"))
+            save.callback(terminal_colorscheme, vim.env.XDG_CONFIG_HOME or (assert(vim.env.HOME) .. "/.config"))
+        end,
+        save_timeout = save.timeout,
+        defer_save = function(self, terminal_colorscheme)
+            if self.save_defer ~= nil then self.save_defer:stop() end
+            self.save_defer = vim.defer_fn(function() self:save(terminal_colorscheme) end, self.save_timeout)
         end,
         get_colorscheme = function(self, colorscheme)
             return get_terminal_colorscheme(colorscheme, self)
@@ -21,16 +32,22 @@ local function new_terminal(id, name, live_update, save)
 end
 
 local terminal_process = {
-    ["wezterm-gui"] = new_terminal("wezterm", "WezTerm", function(terminal_colorscheme)
-        vim.fn.chansend(vim.v.stderr,
-            "\x1b]1337;SetUserVar=neovim_colorscheme=" ..
-            vim.base64.encode(terminal_colorscheme or "") .. "\x07")
-    end, function(terminal_colorscheme, config_home)
-        local dir = config_home .. "/wezterm/generated"
-        vim.fn.mkdir(dir, "p")
-        local data = terminal_colorscheme
-        vim.fn.writefile({ data }, dir .. "/neovim_colorscheme", "")
-    end)
+    ["wezterm-gui"] = new_terminal("wezterm", "WezTerm", {
+        callback = function(terminal_colorscheme)
+            vim.fn.chansend(vim.v.stderr,
+                "\x1b]1337;SetUserVar=neovim_colorscheme=" ..
+                vim.base64.encode(terminal_colorscheme or "") .. "\x07")
+        end,
+        timeout = 0
+    }, {
+        callback = function(terminal_colorscheme, config_home)
+            local dir = config_home .. "/wezterm/generated"
+            vim.fn.mkdir(dir, "p")
+            local data = terminal_colorscheme
+            vim.fn.writefile({ data }, dir .. "/neovim_colorscheme", "")
+        end,
+        timeout = 800
+    })
 }
 
 local terminal = nil
